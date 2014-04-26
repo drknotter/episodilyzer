f3ac49f4d43d4cf383b9d3ce07e60d71faaa27e7
Patrick Plunkett <patplunkett@gmail.com>
2014-04-26 16:59:25 -0700

    Adds an expand/collapse arrow on the right of expandable layouts.

--- src/com/drknotter/episodilyzer/ExpandableLayoutBuilder.java
+++ src/com/drknotter/episodilyzer/ExpandableLayoutBuilder.java
@@ -1,6 +1,7 @@
 package com.drknotter.episodilyzer;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,20 +75,28 @@ class ExpandableLayout extends LinearLayout
 
 		mTitleText.setText(title);
 		mContentText.setText(content);
+		
+		Drawable arrow = getContext().getResources().getDrawable(R.drawable.expand);
+		int size = getContext().getResources().getDimensionPixelSize(R.dimen.expandable_item_height);
+		arrow.setBounds(0, 0, size/2, size/2);
+		mTitleText.setCompoundDrawables(null, null, arrow, null);
 
 		mTitleText.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
 			{
+				Drawable arrow;
 				mContentText.clearAnimation();
 				if( mContentText.getVisibility() == View.GONE )
 				{
 					mContentText.setVisibility(View.VISIBLE);
+					arrow = getContext().getResources().getDrawable(R.drawable.collapse);
 					mContentText.startAnimation(AnimationUtils.loadAnimation(ExpandableLayout.this.getContext(), R.anim.expand_vertical));
 				}
 				else
 				{
+					arrow = getContext().getResources().getDrawable(R.drawable.expand);
 					Animation animation = AnimationUtils.loadAnimation(ExpandableLayout.this.getContext(), R.anim.collapse_vertical);
 					animation.setAnimationListener(new AnimationListener()
 					{
@@ -109,6 +118,9 @@ class ExpandableLayout extends LinearLayout
 					});
 					mContentText.startAnimation(animation);
 				}
+				int size = getContext().getResources().getDimensionPixelSize(R.dimen.expandable_item_height);
+				arrow.setBounds(0, 0, size/2, size/2);
+				mTitleText.setCompoundDrawables(null, null, arrow, null);
 			}
 		});
 	}
