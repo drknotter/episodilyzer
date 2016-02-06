eeefe83930a5ad57436093a52b7c5ba43333f84d
Patrick Plunkett <patplunkett@gmail.com>
2016-02-05 21:00:50 -0800

    Starting to add a series activity.

--- app/src/main/java/com/drknotter/episodilyzer/SeriesActivity.java
+++ app/src/main/java/com/drknotter/episodilyzer/SeriesActivity.java
@@ -1,5 +1,24 @@
 package com.drknotter.episodilyzer;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 public class SeriesActivity extends RecyclerViewActivity {
-    
+    public static final String EXTRA_SERIES_ID = SeriesActivity.class.getCanonicalName() + ".EXTRA_SERIES_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+    }
 }
